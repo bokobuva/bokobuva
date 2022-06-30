35121340d487bf9d6309fe12a2ee9a9308ed3b36
bokobuva <bokobuva@ymail.com>
2022-06-30 16:44:00 +0200

    added few quotes

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
+let apiAuthToken = "ab1c2d6069fd50ef242999ce01ccdf1759989d34dec351821417896daab15ba4c12b494b4bcd7f7ba313b2ea3954e8fe184e112560844da747cfc87276390a0f270f8dfb4e880cbed3049b479a0428dd2253808c7bb6cf8a2134e8da5d028ccbf00d3732d7cd20c46f727f39396869096ca57aadf0a90a9792d4a50d62c4e642";
+
 let allQuotes = document.querySelectorAll("body section h3");
 
+let blogPosts = [];
+
+fetch("http://localhost:1337/api/posts").then(response => console.log(response)).catch(error => console.error(error))
 
 allQuotes[Math.floor(Math.random() * 19)].style.display = "block";
